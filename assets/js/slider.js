064765542fb93519bdc542c19083818cb4493303
mwangdakota <michael.wang@dakota-consulting.com>
2023-07-02 20:43:20 -0400

    Update slider.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -12,21 +12,27 @@ $(function() {
     $($('input.desktop-radio')[3]).click();
   });
 
+  const urlParams = new getAllUrlParams(window.location.search);
+
   $($('input.desktop-radio')[0]).click(function() {
     $('input.mobile-checkbox').removeAttr('checked');
-    $($('input.desktop-radio')[0]).click();
+    $($('input.mobile-checkbox')[0]).click();
+    urlParams.searchParams.set('page', "GetStarted");
   });
   $($('input.desktop-radio')[1]).click(function() {
     $('input.mobile-checkbox').removeAttr('checked');
     $($('input.mobile-checkbox')[1]).click();
+    urlParams.searchParams.set('page', "Qualifications");
   });
   $($('input.desktop-radio')[2]).click(function() {
     $('input.mobile-checkbox').removeAttr('checked');
     $($('input.mobile-checkbox')[2]).click();
+    urlParams.searchParams.set('page', "Submit");
   });
   $($('input.desktop-radio')[3]).click(function() {
     $('input.mobile-checkbox').removeAttr('checked');
     $($('input.mobile-checkbox')[3]).click();
+    urlParams.searchParams.set('page', "GetUpdates");
   });
   //hides down arrows on scroll
   $(".innerScroll").scroll(function() {
