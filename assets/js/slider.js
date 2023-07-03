b8de8ed0017daa5920413fef2fadb83e47468ddf
mwangdakota <michael.wang@dakota-consulting.com>
2023-07-02 21:36:28 -0400

    Update slider.js

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,6 +2,16 @@
 ---
 
 $(function() {
+  //on load go to correct slide
+  const queryString = window.location.search;
+  const slide = new URLSearchParams(queryString).get('slide');
+  if(slide == "qualifications")
+    $($('input.desktop-radio')[1]).click();
+  else if(slide == "submityourpitch")
+    $($('input.desktop-radio')[2]).click();
+  else if(slide == "getupdates")
+    $($('input.desktop-radio')[3]).click();
+
   $('#slide1 .next-button').click(function() {
     $($('input.desktop-radio')[1]).click();
   });
@@ -17,7 +27,7 @@ $(function() {
     $($('input.mobile-checkbox')[0]).click();
     if (window.history.replaceState) {
        //prevents browser from storing history with each change:
-       window.history.replaceState({}, null, "?page=getstarted");
+       window.history.replaceState({}, null, "?slide=getstarted");
     }
   });
   $($('input.desktop-radio')[1]).click(function() {
@@ -25,16 +35,15 @@ $(function() {
     $($('input.mobile-checkbox')[1]).click();
     if (window.history.replaceState) {
        //prevents browser from storing history with each change:
-       window.history.replaceState({}, null, "?page=qualifications");
+       window.history.replaceState({}, null, "?slide=qualifications");
     }
   });
   $($('input.desktop-radio')[2]).click(function() {
-       console.log("buttoncheck");
     $('input.mobile-checkbox').removeAttr('checked');
     $($('input.mobile-checkbox')[2]).click();
     if (window.history.replaceState) {
        //prevents browser from storing history with each change:
-       window.history.replaceState({}, null, "?page=submityourpitch");
+       window.history.replaceState({}, null, "?slide=submityourpitch");
     }
   });
   $($('input.desktop-radio')[3]).click(function() {
@@ -42,8 +51,7 @@ $(function() {
     $($('input.mobile-checkbox')[3]).click();
     if (window.history.replaceState) {
        //prevents browser from storing history with each change:
-       window.history.replaceState({}, null, "?page=getupdates");
-       console.log("replace");
+       window.history.replaceState({}, null, "?slide=getupdates");
     }
   });
   //hides down arrows on scroll
